eb73169ee5635c4214c3e3ca412f91e60757da51
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-06-16 15:23:32 +0200

    feat(modeler): add modeler example
    related to CAM-4079

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ This repository contains a number of examples showing how use and integrate [dmn
 * [simple-commonjs](https://github.com/dmn-io/dmn-js-examples/tree/master/simple-commonjs) - an example showcasing how to load dmn-js via [npm](http://npmjs.org) in a node-style application and package it and the application code for the browser with [browserify](http://browserify.org).
 
 
+## Intermediate
+
+* [modeler](https://github.com/dmn-io/dmn-js-examples/tree/master/modeler) - a simple DMN modeler. Gains more features as time passes by.
+
 ## License
 
 MIT _(unless noted otherwise)_
---
+++ modeler/app/index.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var fs = require('fs');
+
+var $ = require('jquery'),
+    DmnModeler = require('dmn-js/lib/Modeler');
+
+var container = $('#js-drop-zone');
+
+var canvas = $('#js-table');
+
+var renderer = new DmnModeler({ container: canvas });
+
+var newTableXML = fs.readFileSync(__dirname + '/../resources/newTable.dmn', 'utf-8');
+
+function createNewTable() {
+  openTable(newTableXML);
+}
+
+function openTable(xml) {
+
+  renderer.importXML(xml, function(err) {
+
+    if (err) {
+      container
+        .removeClass('with-table')
+        .addClass('with-error');
+
+      container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-table');
+    }
+
+
+  });
+}
+
+function saveTable(done) {
+
+  renderer.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
+}
+
+function registerFileDrop(container, callback) {
+
+  function handleFileSelect(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var files = e.dataTransfer.files;
+
+    var file = files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+
+      var xml = e.target.result;
+
+      callback(xml);
+    };
+
+    reader.readAsText(file);
+  }
+
+  function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  container.get(0).addEventListener('dragover', handleDragOver, false);
+  container.get(0).addEventListener('drop', handleFileSelect, false);
+}
+
+
+////// file drag / drop ///////////////////////
+
+// check file api availability
+if (!window.FileList || !window.FileReader) {
+  window.alert(
+    'Looks like you use an older browser that does not support drag and drop. ' +
+    'Try using Chrome, Firefox or the Internet Explorer > 10.');
+} else {
+  registerFileDrop(container, openTable);
+}
+
+// bootstrap table functions
+
+$(document).on('ready', function() {
+
+  $('#js-create-table').click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    createNewTable();
+  });
+
+  var downloadLink = $('#js-download-table');
+
+  $('.buttons a').click(function(e) {
+    if (!$(this).is('.active')) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+  function setEncoded(link, name, data) {
+    var encodedData = encodeURIComponent(data);
+
+    if (data) {
+      link.addClass('active').attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+        'download': name
+      });
+    } else {
+      link.removeClass('active');
+    }
+  }
+
+  var exportArtifacts = window.setInterval(function() {
+    saveTable(function(err, xml) {
+      setEncoded(downloadLink, 'table.dmn', err ? null : xml);
+    });
+
+  }, 1000);
+
+  renderer.on('commandStack.changed', exportArtifacts);
+});
