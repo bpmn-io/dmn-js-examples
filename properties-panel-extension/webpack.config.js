2d8fcfb0479fdc4787e9d46cd7484ba077c7cac6
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-10-13 15:20:55 +0200

    feat: add properties panel extension example

---
+++ properties-panel-extension/webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.bundled.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.dmn$/,
+        type: 'asset/source'
+      }
+    ]
+  }
+};
